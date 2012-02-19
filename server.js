f4e60f8b7ce3b9c26c0b4d7973c30ec88169d580
Duana Stanley <strongcoffee2@gmail.com>
2012-02-19 22:51:39 +0100

    show filename after uploading

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var formidable = require('formidable');
 var util = require('util');
+var uuid = require('node-uuid');
+
+var TMP_UPLOAD_DIR = "tmp";
+var UPLOAD_DIR = "uploads";
 
 var app = express.createServer();
 
 app.configure(function(){
     app.set('view engine', 'jade');
     app.set('view options', { layout: false });
-  //  app.use(express.bodyParser());
 });
 
 app.get('/', function(req, res){
@@ -17,10 +20,18 @@ app.get('/', function(req, res){
 
 app.post('/', function(req, res){
     var form = new formidable.IncomingForm();
+    form.uploadDir = TMP_UPLOAD_DIR;
     form.parse(req, function(err, fields, files) {
       res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
+      // res.end(util.inspect({fields: fields, files: files}));
+      var filename = UPLOAD_DIR + "/" + uuid.v1();
+      fs.rename(files.file.path, filename, function(e){
+        if(e){
+            res.end("exception: " + e);
+        }
+        res.end("file on server: " + filename);
+      });
+
     });
 });
 
